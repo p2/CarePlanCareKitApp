df2d1f39330b53b660260cc12584d0d0571fdb54
Pascal Pfiffner <phase.of.matter@gmail.com>
2016-05-06 11:09:52 -0400

    Initial Commit

---
+++ CarePlan/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CarePlan
+//
+//  Created by Pascal Pfiffner on 06/05/16.
+//  Copyright © 2016 CHIP. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
